5272b9464aa61c4d8e1c62960e597dcc08c59aea
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2024-04-01 09:57:12 +0200

    20240401

---
+++ WpfBoundsTestProps/Release/LV_Test.lvproj
@@ -0,0 +1,23 @@
+﻿<?xml version='1.0' encoding='UTF-8'?>
+<Project Type="Project" LVVersion="24008000">
+	<Property Name="NI.LV.All.SourceOnly" Type="Bool">true</Property>
+	<Item Name="My Computer" Type="My Computer">
+		<Property Name="server.app.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="server.control.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="server.tcp.enabled" Type="Bool">false</Property>
+		<Property Name="server.tcp.port" Type="Int">0</Property>
+		<Property Name="server.tcp.serviceName" Type="Str">My Computer/VI Server</Property>
+		<Property Name="server.tcp.serviceName.default" Type="Str">My Computer/VI Server</Property>
+		<Property Name="server.vi.callsEnabled" Type="Bool">true</Property>
+		<Property Name="server.vi.propertiesEnabled" Type="Bool">true</Property>
+		<Property Name="specify.custom.address" Type="Bool">false</Property>
+		<Item Name="Test.vi" Type="VI" URL="../Test.vi"/>
+		<Item Name="Dependencies" Type="Dependencies">
+			<Item Name="System.Drawing" Type="Document" URL="System.Drawing">
+				<Property Name="NI.PreserveRelativePath" Type="Bool">true</Property>
+			</Item>
+			<Item Name="WindowsFormsControlLibrary.dll" Type="Document" URL="../WindowsFormsControlLibrary.dll"/>
+		</Item>
+		<Item Name="Build Specifications" Type="Build"/>
+	</Item>
+</Project>
---
+++ WpfBoundsTestProps/WindowsFormsControlLibrary/UserControl1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary
+{
+    public partial class UserControl1: UserControl
+    {
+		// Private data member that backs the EndColor property.  
+		private Color myborderColor = Color.LimeGreen;
+		// The Category attribute tells the designer to display  
+		// it in the Flash grouping.
+		// The Description attribute provides a description of  
+		// the property.
+		[
+		Category("Flash"),
+		Description("The ending color of the bar.")
+		]
+
+		public Color MyBorderColor
+		{
+			get { return myborderColor; }
+			set {
+				myborderColor = value;
+				label1.ForeColor = value;
+				//UserControl1.MyColorProperty = value;
+				//if (baseBackground != null && showGradient){
+				//	baseBackground.Dispose();
+				//	baseBackground = null;
+				//}
+				// The Invalidate method calls the OnPaint method, which redraws
+				// the control.  
+				Invalidate();
+			}
+		}
+
+		public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+		private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+		{
+
+		}
+	}
+}
